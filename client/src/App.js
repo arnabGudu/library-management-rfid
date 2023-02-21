bd6beae8b1ca4ca2139e821a16e642b0511f3dae
arnabGudu <arnabgudu.gs@gmail.com>
2023-02-22 00:46:48 +0530

    added react client

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react"
+import BooksTable from "./components/BooksTable"
+
+const App = () => {
+  return (
+    <div className="App">
+      <BooksTable />
+    </div>
+  )
+}
+export default App
